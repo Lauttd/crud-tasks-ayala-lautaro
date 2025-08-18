7a886e2db36b6e46340e1738c1430f4b4ea504d0
Lauttd <lautaroayala123090@gmail.com>
2025-08-18 11:59:28 -0300

    se hizo las rutas de alumno y profesor

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ import dotenv from "dotenv";
 import { initDB } from "./src/config/database.js";
 import { routerTask } from "./src/routes/task.routes.js";
 import { routerUsers } from "./src/routes/users.routes.js";
+import { routerAlumno } from "./src/routes/alumnos.routes.js";
+import { routerProfesores } from "./src/routes/profesores.routes.js";
 
 
 dotenv.config();
@@ -13,6 +15,8 @@ const PORT = process.env.PORT;
 app.use(express.json());
 app.use("/", routerTask);
 app.use("/", routerUsers);
+app.use("/", routerAlumno);
+app.use("/", routerProfesores);
 
 initDB().then(() => {
     app.listen(PORT, () => {
